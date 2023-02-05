00713c71f80a06fecaf41a70b0072d2ae4e4ac07
reihane sheikheezade <sheikhe287@gmail.com>
2023-02-05 04:25:45 -0800

    lift category and products states to App component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
+import { useState } from "react";
 import CategoryForm from "./Component/CategoryForm";
 import Navbar from "./Component/Navbar";
 import ProductsForm from "./Component/ProductsForm";
 
-
-
 function App() {
+  const [categories, setCategories] = useState([]);
+  const [products, setProducts] = useState([]);
+
   return (
     <div className="min-h-screen bg-slate-800">
-    {/* Navbar */}
-    <Navbar />
-    {/* app content */}
-    <div className="container max-w-screen-sm m-auto">
-      <CategoryForm />
-      <ProductsForm />
-    </div>
+      {/* Navbar */}
+      <Navbar />
+      {/* app content */}
+      <div className="container max-w-screen-sm m-auto">
+        <CategoryForm setCategories={setCategories} />
+        <ProductsForm categories={categories} setProducts={setProducts} />
+      </div>
     </div>
   );
 }
