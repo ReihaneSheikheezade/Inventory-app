76cae1e9c8d6f252df43699562d1e56b3bf6a038
reihane sheikheezade <sheikhe287@gmail.com>
2023-02-13 03:46:25 -0800

    styles changed

--- src/Component/Filter.js
+++ src/Component/Filter.js
@@ -4,11 +4,11 @@ const Filter = ({onSort,onSearch ,sort,searchValue, categories ,onSelectCatogory
   
   return (
     <div>
-      <div className="flex justify-between items-center mb-6">
+      <div className="flex justify-between items-center mb-2">
         <label htmlFor="search-input" className="text-slate-500 sm:text-lg">search</label>
         <input type="text" onChange={onSearch} value={searchValue} id="search-input" name="search-input" className="rounded-xl p-2 bg-transparent border border-slate-500 text-slate-400" />
       </div>
-      <div className="flex justify-between items-center mb-6 ">
+      <div className="flex justify-between items-center mb-2 ">
         <label htmlFor="sort-products" className="text-slate-500 sm:text-lg">sort</label>
         <select onChange={onSort} value={sort} id="sort-products" name="sort-products"  className="bg-transparent text-slate-400 rounded-xl" >
           <option className="bg-slate-500 text-slate-300" value="">select a ...</option>
